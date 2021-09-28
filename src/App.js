6969290d1bc6a8bb6ab54f9808a4ef7c0d994460
BHessel <hesselfitness@gmail.com>
2021-09-28 11:19:23 -0400

    added todolist component + inputs/buttons on App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import TodoList from './TodoList'
 
 function App() {
-  return null
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>
+        0 tasks remaining
+        {/* I can clean this up with a ternary statement later, to read 1 task instead of tasks when tasksRemaining === 1 */}
+      </div>
+    </>
+  )
 }
 
 export default App;
