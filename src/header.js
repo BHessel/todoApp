7adc21d88f75ecaca1d0a48299a054840040393c
Ben Hessel <hesselfitness@gmail.com>
2022-07-14 11:54:00 -0400

    save, branch to make About modal

--- src/header.js
+++ src/header.js
@@ -1,36 +1,46 @@
-import React from 'react'
-
+import React from "react";
 
 export default function Header() {
-    return (
-        <div className='header'>
-            <div className='title'>
-                <a href='https://opensea.io/collection/lonelyalienspaceclub' target='_blank'>Lonely Alien Todo List</a>
-            </div>
-            <div className='options'>
-                <ul className='head-links'>
-                    <li>
-                        <a href='https://opensea.io/collection/lonelyalienspaceclub' target='_blank'>About
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://github.com/BHessel/todoApp' target='_blank'>Github
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div className='bs'>
-                <button>Fake Button!</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="title">
+        <a
+          href="https://opensea.io/collection/lonelyalienspaceclub"
+          target="_blank"
+        >
+          Lonely Alien Todo List
+        </a>
+      </div>
+      <div className="options">
+        <ul className="head-links">
+          <li>
+            <a
+              href="https://opensea.io/collection/lonelyalienspaceclub"
+              target="_blank"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/BHessel/todoApp" target="_blank">
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="bs">
+        <button onClick={() => alert("Told you it was fake! :)")}>
+          Fake Button!
+        </button>
+      </div>
+    </div>
+  );
 }
 
 //Make left side with logo
-//Make right side with Login (jk, practice app)
+//Make right side with Login
 //import a cool font?
 //make background cool in some way
-
 
 //for input area, center everything, then:
 //hover for buttons to show when highlighted/clicked
